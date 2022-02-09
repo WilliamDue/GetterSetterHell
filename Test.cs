a17ee19b8fa7f8a2c969c5272974d0504fb3b7c0
William Due <williamhenrichdue@gmail.com>
2022-02-09 21:56:27 +0100

    added comments

--- Test.cs
+++ Test.cs
@@ -3,10 +3,21 @@ namespace GetterSetterHell;
 public class Test {
     
     private string firstThing;
+
+    // This property behaves identically to a public field.
     public string SecondThing { get; set; }
+
+    // This behaves like a public field but one is only able to set its value inside the class.
     public string PrivateSetter { get; private set; }
+
+    // This makes the property readonly.
     public string OnlyGettable { get; }
 
+    // This does not work a getter must be present.
+    // public string OnlySettable { set; }
+
+    // This property behaves the same as the the functions GetFirstThing and SetFirstThing.
+    // The only difference is the syntax.
     public string FirstThing {
 
         get {
@@ -21,7 +32,10 @@ public class Test {
 
     }
 
-    // You don't have to use this.
+    // This property behaves the same as the FirstThing property, the difference is it does not 
+    // print to console. It works if the setter/getter is a one-liner (It can also be used for 
+    // methods). This syntax might be confusing but if you meet it when researching you can see it 
+    // here.
     public string FirstThingLambdaExpression {
         get => firstThing;
         set => firstThing = value;
@@ -35,6 +49,7 @@ public class Test {
         OnlyGettable = thing;
     }
 
+    // The methods GetFirstThing/SetFirstThing behaves the same as the FirstThing property.
     public string GetFirstThing() {
         Console.WriteLine("You are getting first thing.");
         return firstThing;
